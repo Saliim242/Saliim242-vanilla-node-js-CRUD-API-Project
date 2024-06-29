e9cc40ffd9e49dc7817610333843924f99b806a5
Saliim242 <github242A@gmail.com>
2024-06-29 06:01:54 +0300

    Adding Post

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,13 @@
-module.exports = (req, res) => {};
+const crypto = require("crypto");
+const requestBodyParser = require("../utils/body-parse");
+
+module.exports = async (req, res) => {
+  if (req.url === "/api/movies") {
+    try {
+      let body = await requestBodyParser(req.body);
+      console.log(body);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
